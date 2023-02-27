41b35e98f84608fe38b94c1c1cbaadd9c10324f3
dejwid <dawid.paszko@gmail.com>
2023-02-27 11:17:56 +0100

    vercel 2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,7 +13,7 @@ import PlacePage from "./pages/PlacePage";
 import BookingsPage from "./pages/BookingsPage";
 import BookingPage from "./pages/BookingPage";
 
-axios.defaults.baseURL = 'http://127.0.0.1:4000';
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
 axios.defaults.withCredentials = true;
 
 function App() {
