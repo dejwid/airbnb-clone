45be0f44904a39ec8555c6a678f3a115db8a1017
Debayan <132012594+felixoder@users.noreply.github.com>
2024-01-27 01:15:50 +0530

    solution for the CORS error
    I have added the react home origin to solve the CORS error btw awesome project

--- api/index.js
+++ api/index.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use('/uploads', express.static(__dirname+'/uploads'));
 app.use(cors({
   credentials: true,
-  origin: 'http://127.0.0.1:5173',
+  origin: 'https://airbnb-clone-dejwid.vercel.app',
 }));
 
 async function uploadToS3(path, originalFilename, mimetype) {
